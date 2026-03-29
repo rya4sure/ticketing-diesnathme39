6bc080213524499016af6ec7a1d2d518f1026c07
rya <80244894+rya4sure@users.noreply.github.com>
2026-03-29 21:08:28 +0700

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+  supabase: {
+    url: 'https://noebfxwfwwarutmezzxk.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5vZWJmeHdmd3dhcnV0bWV6enhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ3MTM3NTUsImV4cCI6MjA5MDI4OTc1NX0.naGN5u0z0kWOcEfgCXubR4ThU_tY2na-g1JDjDVS64I'
+  },
+  resend: {
+    apiKey: 're_xxxxxxxxx'
+  }
+}
